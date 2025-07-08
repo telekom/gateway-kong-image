ae9165b4d548c7ff52e4a13732a68c04bc1f5b57
luis.pflamminger@telekom.de <luis.pflamminger@telekom.de>
2025-07-08 16:30:51 +0200

    ci: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+    branches: ['main'],
+    tagFormat: '${version}',
+    repositoryUrl: 'git@github.com:telekom/gateway-kong-image.git',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        'semantic-release-export-data',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+    ],
+};
